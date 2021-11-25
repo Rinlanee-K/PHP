26bc5b91001205fc737963e4742edf3dc59bfa8b
AzureAD\RinlaneeKhwannak <46128@gymnasium.ax>
2021-11-25 11:52:40 +0200

    La in övriga filer i mappen Övrigt i PHPovningargit

---
+++ Övrigt/valjDryck.php
@@ -0,0 +1,11 @@
+<?php
+declare(strict_types=1);
+	function valjDryck(string $dryck='Vatten') {
+		return "Vald dryck är $dryck.";
+	}
+	
+	echo valjDryck();
+	echo valjDryck('Te');
+	echo valjDryck(null);
+	
+	

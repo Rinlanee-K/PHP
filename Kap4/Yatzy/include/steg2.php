07f202bf4a5ca8557c894c3001dc91044de84273
AzureAD\RinlaneeKhwannak <46128@gymnasium.ax>
2021-11-25 12:06:20 +0200

    La till yatzy i Kap4

---
+++ Kap4/Yatzy/include/steg2.php
@@ -0,0 +1,13 @@
+<p>Du har slagit alla slagen. Dina tärningar visar:<br>
+    <?php
+    foreach ($tarning as $key => $value) {
+        echo "<div class='tarningarna'> <img src='bilder/dice_$value.png' alt='Tärning_$value'></div>";
+    }
+    ?>
+    <br>
+    Du fick en <?= $resultat['resultat']; ?> med värdet <?= $resultat['varde']; ?>
+<form method="POST">
+    <input type="submit" name="submit" value="Spela igen">
+    <input type="submit" name="submit" value="Göra något tråkigt">
+</form>
+

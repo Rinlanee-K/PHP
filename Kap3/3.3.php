5da14572d3c8f9473cf8bb51b5be33e8a81977d7
AzureAD\RinlaneeKhwannak <46128@gymnasium.ax>
2021-11-25 11:35:09 +0200

    La in kap3 filer i Kap3 PHPovningargit

---
+++ Kap3/3.3.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Jämför inmatade värden</title>
+	</head>
+		<h1>Jämför värden</h1>
+		<form method="POST">
+			Värde 1: <input type="number" name="varde1"><br>
+			Värde 2: <input type="number" name="varde2"><br>
+			<input type="submit" value="Skicka">	
+		</form>

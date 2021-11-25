65308093cb9a54ef83df6c15824f6b9ae113bae1
AzureAD\RinlaneeKhwannak <46128@gymnasium.ax>
2021-11-25 11:52:27 +0200

    La in övriga filer i mappen Övrigt i PHPovningargit

---
+++ Övrigt/tjejvasan.php
@@ -0,0 +1,50 @@
+<?php
+declare(strict_types=1);
+//Ange ett standardvärde
+$artal=date("Y");
+
+//Kontrollera om vi fått någon indata
+if(isset($_GET['artal'])) {
+	//Ta hand om indata om det är ett heltal
+	$artal=filter_input(INPUT_GET, 'artal', FILTER_VALIDATE_INT);
+	//Sätt årets år om talet är utanför angivna gränser
+	if ($artal>2050 || $artal<1988){
+		$artal=date("Y");
+	}
+}
+?>
+
+<!DOCTYPE html>
+<html lang="sv">
+    <head>
+	<h1>Tjejvasan</h1>
+        <meta charset="UTF-8">
+        <title>Tjejvasan (4.23)</title>
+    </head>
+    <body>
+        <form method="GET">
+            Ange år: <input type="number" value="<?= $artal; ?>" name="artal" max=2050 min=1988><br>
+            <input type="submit" name="submit" value="Skicka">
+        </form>
+		<p>Tjejvasan genomförs de närmsta 5 åren följande datum:<br>
+		<?php
+			for($i=0;$i<5;$i++) {
+				echo $artal+$i . " den " . tjejvasan($artal+$i) . "<br>";
+			}
+		?>
+		</p>
+	</body>
+</html>
+<?php
+//Funktion för att beräkna datum för tjejvasan
+//Sträng med datum som returneras
+function tjejvasan(int $ar) :string {
+	//Hitta datum för vanliga Vasaloppet
+	$datum=strtotime("First Sunday of March $ar");
+	//Tjejvasan går lördagen veckan före vanliga Vasaloppet
+	$datum=strtotime("-8 days", $datum);
+	
+	//Returnera det funna datumet
+	return date("Y-m-d", $datum);
+}
+?>
